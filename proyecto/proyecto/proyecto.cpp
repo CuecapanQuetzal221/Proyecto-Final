8d8a84f93065eba659651a6757757330ac14b51a
CuecapanQuetzal221 <bruno.cuecapan.enp5.221@gmail.com>
2022-11-29 17:05:45 -0600

    pase todos los cambios en second a main

--- proyecto/proyecto/proyecto.cpp
+++ proyecto/proyecto/proyecto.cpp
@@ -5,13 +5,53 @@ int main()
 
     scanf_s("%i %i %i", &dia, &mes, &anio);
 
-    if ((anio % 4 == 0) && (anio % 100 != 0 || anio % 400 == 0))
+    if ((anio % 4 == 0) && (anio % 100 != 0 || anio % 400 == 0)) 
     {
-        if (mes == 2)
+        if (mes == 2) 
         {
-            if (dia != 29)
+            if (dia != 29) 
+            {
                 dia++;
+            }
+            else 
+            {
+                mes++;
+                dia = 1;
+            }
+        }
+        else if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 ||
+            mes == 10 || mes == 12) 
+        {
+
+            if (dia == 31 && mes != 12) 
+            {
+                mes++;
+                dia = 1;
+            }
+            else if (dia == 31 && mes == 12) 
+            {
+                dia = 1;
+                mes = 1;
+                anio++;
+            }
+            else 
+            {
+                dia++;
+            }
 
         }
+        else 
+        {
+            if (dia == 30) 
+            {
+                mes++;
+                dia = 1;
+            }
+            else 
+            {
+                dia++;
+            }
+        }
+
     }
-};
+}
