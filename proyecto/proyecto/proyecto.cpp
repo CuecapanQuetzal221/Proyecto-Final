b7f7c7b0de2e2f434c0f8387bfdb2805ddfaf08c
CuecapanQuetzal221 <bruno.cuecapan.enp5.221@gmail.com>
2022-11-29 16:00:35 -0600

    primera sentencia

---
+++ proyecto/proyecto/proyecto.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int dia, mes, anio;
+
+    scanf_s("%i %i %i", &dia, &mes, &anio);
+
+    if ((anio % 4 == 0) && (anio % 100 != 0 || anio % 400 == 0))
+    {
+        if (mes == 2)
+        {
+            if (dia != 29)
+                dia++;
+
+        }
+    }
+};
